477033f1df2029722158bcb0c2519990ec1c0525
Michael Sullivan <104106828+ms13380@users.noreply.github.com>
2022-10-05 23:51:33 -0400

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,29 @@
+
+import { Workbox } from 'workbox-window';
+import Editor from './editor';
+import './database';
+import './style.css';
+const main = document.querySelector('#main');
+main.innerHTML = '';
+const loadSpinner = () => {
+	const spinner = document.createElement('div');
+	spinner.classList.add('spinner');
+	spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+	main.appendChild(spinner);
+};
+const editor = new Editor();
+if (typeof editor === 'undefined') {
+	loadSpinner();
+}
+if ('serviceWorker' in navigator) {
+	// register workbox service worker
+	const workboxSW = new Workbox('/service-worker.js');
+	workboxSW.register();
+	console.log('SW registered');
+} else {
+	console.error('Service workers are not supported in this browser.');
+}
